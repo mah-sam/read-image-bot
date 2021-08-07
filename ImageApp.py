bf031f0932294ca7c025d7290da012d5647202c2
mah-sam <82774404+mah-sam@users.noreply.github.com>
2021-08-08 00:57:21 +0300

    comments added

--- ImageApp.py
+++ ImageApp.py
@@ -3,6 +3,7 @@ import discord
 from functions import *
 
 
+# Configuration
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 bot = discord.Client()
@@ -10,6 +11,7 @@ bot = discord.Client()
 
 @bot.event
 async def on_ready():
+    '''Notify the readiness of the bot'''
     print(
         f'{bot.user} is connected'
     )
@@ -17,9 +19,11 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    '''Runs everytime a message is sent to detect a mention'''
     if message.author == bot.user:
         return
-
+    
+    # Check if bot id in message for a ping
     if "873485624034877481" in message.content:  
         print("Mention detected".center(100, '-'))
         if 'ar' in message.content:
@@ -27,13 +31,16 @@ async def on_message(message):
         else:
             lang = 'eng'
         
+        # Look up the last 200 messages in the channel
         async for mes in message.channel.history(limit=200):
             if mes.attachments:
+                # get the last attachment
                 url = str(mes.attachments[-1])
                 if is_image(url):
                     print("Last image in channel detected".center(100, '-'))
                     try:
                         print("Getting OCR response".center(100, '-'))
+                        # Get image text and replace probably mistaken chars
                         response = process_image(url, lang).replace("|", "I").replace("\n", " ")
                         print("Text was successfully processed".center(100, '-'))
                         await message.channel.send(response, tts=True)
